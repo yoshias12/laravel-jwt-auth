dbc84bebfb85fe8d23ef1d47cc82080247c3ec68
Rizka Andhika Mulya Sejati <riksaandika7@gmail.com>
2024-08-10 19:25:50 +0700

    final

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,6 +45,7 @@ class AuthController extends Controller
         }
 
         return response()->json([
+            'email' => $request->email,
             'token' => $token
         ]);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,9 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'email' => $this->email
+        ];
     }
 
     /**
